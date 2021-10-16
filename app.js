3e95cb3f764f3045fc6172e0345befe21593863a
kirill <kirillwork56@gmail.com>
2021-10-16 15:23:08 +0300

    Added paddle and keyboard controls

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+//Canvas var =========================================================
+
 let canvas = document.querySelector("#myCanvas");
 let ctx = canvas.getContext("2d");
+
+//Variables ==========================================================
 
 let x = canvas.width / 2;
 let y = canvas.height - 30;
@@ -9,6 +13,37 @@ let dy = -2;
 
 let ballRadius = 10;
 
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+let rightPressed = false;
+let leftPressed = false;
+
+//Control ============================================================
+
+const keyDownHandler = (e) => {
+  debugger;
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = true;
+  } else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = true;
+  }
+};
+
+const keyUpHandler = (e) => {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = false;
+  } else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = false;
+  }
+};
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+//Function ===========================================================
+
 const drawBall = (params) => {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -17,18 +52,43 @@ const drawBall = (params) => {
   ctx.closePath();
 };
 
+const drawPaddle = () => {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+};
+
 const draw = (params) => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
-  x += dx;
-  y += dy;
+  drawPaddle();
 
+  //Canvas border check
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
   if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
     dy = -dy;
   }
+
+  //Button check
+  if (rightPressed) {
+    paddleX += 7;
+    if (paddleX + paddleWidth > canvas.width) {
+      paddleX = canvas.width - paddleWidth;
+    }
+  } else if (leftPressed) {
+    paddleX -= 7;
+    if (paddleX < 0) {
+      paddleX = 0;
+    }
+  }
+
+  x += dx;
+  y += dy;
 };
 
+//Function start ========================================================
 setInterval(draw, 10);
