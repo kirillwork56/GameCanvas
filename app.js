cb5e3ed61224775f210b37b1caffb4618fd9f1e3
kirill <kirillwork56@gmail.com>
2021-10-16 19:16:02 +0300

    Ended game

--- app.js
+++ app.js
@@ -20,10 +20,32 @@ let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
 
-//Control panel ====================================================
+let score = 0;
+let lives = 5;
+
+//Brick variables ==================================================
+
+let brickRowCount = 3;
+let brickColumnCount = 5;
+
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+
+let bricks = [];
+for (let c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (let r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
+  }
+}
+
+//Control =============================================================
 
 const keyDownHandler = (e) => {
-  debugger;
   if (e.key == "Right" || e.key == "ArrowRight") {
     rightPressed = true;
   } else if (e.key == "Left" || e.key == "ArrowLeft") {
@@ -39,12 +61,46 @@ const keyUpHandler = (e) => {
   }
 };
 
+const mouseMoveHandler = (e) => {
+  let relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2;
+  }
+};
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
 //Function ===========================================================
 
-const drawBall = (params) => {
+const collisionDetection = () => {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      let b = bricks[c][r];
+      if (b.status == 1) {
+        if (
+          x > b.x &&
+          x < b.x + brickWidth &&
+          y > b.y &&
+          y < b.y + brickHeight
+        ) {
+          dy = -dy;
+          b.status = 0;
+          score++;
+          if (score == brickRowCount * brickColumnCount) {
+            alert("YOU WIN, CONGRATULATIONS!");
+            document.location.reload();
+          }
+        }
+      }
+    }
+  }
+};
+
+// draw function =====================================================
+
+const drawBall = () => {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD";
@@ -60,10 +116,49 @@ const drawPaddle = () => {
   ctx.closePath();
 };
 
-const draw = (params) => {
+const drawBricks = () => {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      if (bricks[c][r].status == 1) {
+        let brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+        let brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+  }
+};
+
+const drawScore = () => {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Score: " + score, 8, 20);
+};
+
+const drawLives = () => {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
+};
+
+//Main function ========================================================
+
+const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBricks();
   drawBall();
   drawPaddle();
+  drawScore();
+  drawLives();
+
+  debugger;
+
+  collisionDetection();
 
   //Canvas border check
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
@@ -75,9 +170,17 @@ const draw = (params) => {
     if (x > paddleX && x < paddleX + paddleWidth) {
       dy = -dy;
     } else {
-      alert("GAME OVER");
-      document.location.reload();
-      clearInterval(interval);
+      lives--;
+      if (!lives) {
+        alert("GAME OVER");
+        document.location.reload();
+      } else {
+        x = canvas.width / 2;
+        y = canvas.height - 30;
+        dx = 2;
+        dy = -2;
+        paddleX = (canvas.width - paddleWidth) / 2;
+      }
     }
   }
 
@@ -96,7 +199,6 @@ const draw = (params) => {
 
   x += dx;
   y += dy;
+  requestAnimationFrame(draw);
 };
-
-//Function start ========================================================
-let interval = setInterval(draw, 10);
+draw();
