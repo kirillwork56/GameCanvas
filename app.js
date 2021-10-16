348cd783f940ec4e6886da6de8d07fda9d12daf4
kirill <kirillwork56@gmail.com>
2021-10-16 14:45:03 +0300

    Added bounce off the walls

--- app.js
+++ app.js
@@ -7,19 +7,28 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
-function drawBall() {
+let ballRadius = 10;
+
+const drawBall = (params) => {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
-}
+};
 
-function draw() {
+const draw = (params) => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
   x += dx;
   y += dy;
-}
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
+};
 
 setInterval(draw, 10);
